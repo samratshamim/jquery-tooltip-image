74ef902acb9afcaddcb0cdab834be6aa40b359b8
Shamim Reza <shamim@optimumservices.com.bd>
2023-02-22 03:22:32 +0600

    Added auto floating to left and right based on mousepoint xOffset | Image max height adjusted

--- imagepreview.js
+++ imagepreview.js
@@ -3,7 +3,7 @@
     a.fn.SamratShamimToolTip = function(b) {
         var c = a.extend({
             xOffset: 10,
-            yOffset: 30
+            yOffset: 10
         },
         b);
         return this.each(function() {
@@ -12,7 +12,7 @@
                 this.t = this.title,
                 this.title = "";
                 var d = "" != this.t ? "<br/>" + this.t : "";
-                a("body").append("<p id='preview'><img src='" + this.rel + "' alt='Image preview' />" + d + "</p>"),
+                a("body").append("<p id='preview'><img id='prev' src='" + this.rel + "' alt='Image preview' />" + d + "</p>"),
                 a("#preview").css({
                     top: b.pageY - c.xOffset + "px",
                     left: b.pageX + c.yOffset + "px"
@@ -23,7 +23,16 @@
                 a("#preview").remove()
             }),
             b.mousemove(function(b) {
-                a("#preview").css("top", b.pageY - c.xOffset + "px").css("left", b.pageX + c.yOffset + "px")
+                // console.log(b.pageX);
+                // console.log($("#preview").width());
+                // console.log($(window).width());
+                var ful = $(window).width();
+                var half = ($(window).width() / 2);
+                $("#prev").css("width", half - 30 + "px")
+                if(b.pageX < half)
+                    a("#preview").css("top", b.pageY - c.xOffset + "px").css("left", b.pageX + c.yOffset + "px")
+                else
+                    a("#preview").css("top", b.pageY - c.xOffset + "px").css("right", ful - b.pageX + "px").css("left", "auto")
             })
         })
     }
